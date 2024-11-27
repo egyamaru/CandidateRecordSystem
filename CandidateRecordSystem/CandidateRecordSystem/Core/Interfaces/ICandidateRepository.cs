4d0f5bcf391975b6576d1d972b39fcd1af991309
elish <elishgyamaru@outlook.com>
2024-11-27 18:09:29 +0545

    create repository

---
+++ CandidateRecordSystem/CandidateRecordSystem/Core/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,9 @@
+﻿using CandidateRecordSystem.Core.Entities;
+
+namespace CandidateRecordSystem.Core.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<Candidate> InsertOrUpdate(Candidate candidate);
+    }
+}
---
+++ CandidateRecordSystem/CandidateRecordSystem/Infrastructure/Repositories/CandidateRepository.cs
@@ -0,0 +1,45 @@
+﻿using CandidateRecordSystem.Core.Entities;
+using CandidateRecordSystem.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateRecordSystem.Infrastructure.Repositories
+{
+    public class CandidateRepository:ICandidateRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CandidateRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Candidate> InsertOrUpdate(Candidate candidate)
+        {
+
+            var existingCandidate= await _appDbContext.Candidate.FirstOrDefaultAsync(c=>c.Email == candidate.Email);
+
+            if( existingCandidate is null)
+            {
+                //insert
+                await _appDbContext.AddAsync(candidate);
+                await _appDbContext.SaveChangesAsync();
+            }
+            else
+            {
+                //update
+                existingCandidate.PhoneNumber = candidate.PhoneNumber;
+                existingCandidate.FirstName = candidate.FirstName;
+                existingCandidate.LastName = candidate.LastName;
+                existingCandidate.Email = candidate.Email;
+                existingCandidate.Comments = candidate.Comments;
+                existingCandidate.GithubUrl = candidate.GithubUrl;
+                existingCandidate.LinkedInProfileUrl = candidate.LinkedInProfileUrl;
+
+                await _appDbContext.SaveChangesAsync();
+                candidate.CandidateId = existingCandidate.CandidateId;
+            }
+            
+            return candidate;
+        }
+    }
+}
