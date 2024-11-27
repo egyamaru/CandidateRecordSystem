16be8353320661c5b00b3709dd4c0bd83d64ac53
elish <elishgyamaru@outlook.com>
2024-11-27 18:06:52 +0545

    make preferred time to call nullable

--- CandidateRecordSystem/CandidateRecordSystem/Migrations/20241127122051_makePreferredTimeToCallNullable.Designer.cs
+++ CandidateRecordSystem/CandidateRecordSystem/Migrations/20241127122051_makePreferredTimeToCallNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CandidateRecordSystem.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CandidateRecordSystem.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241127122051_makePreferredTimeToCallNullable")]
+    partial class makePreferredTimeToCallNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.36");
---
+++ CandidateRecordSystem/CandidateRecordSystem/Migrations/20241127122051_makePreferredTimeToCallNullable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CandidateRecordSystem.Migrations
+{
+    public partial class makePreferredTimeToCallNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "PreferredTimeToCall",
+                table: "Candidate",
+                type: "TIME",
+                nullable: true,
+                oldClrType: typeof(TimeOnly),
+                oldType: "TIME");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "PreferredTimeToCall",
+                table: "Candidate",
+                type: "TIME",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0),
+                oldClrType: typeof(TimeOnly),
+                oldType: "TIME",
+                oldNullable: true);
+        }
+    }
+}
